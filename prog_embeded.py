89ea6d915e5db0263663f02de4bb33cf8cdbe6b8
MikaelDv <enzomikael@hotmail.com>
2024-09-20 23:13:00 -0300

    parte 1

--- prog_embeded.py
+++ prog_embeded.py
@@ -7,54 +7,68 @@ embed = hub.load("https://www.kaggle.com/models/google/universal-sentence-encode
 
 lista = lista_frases.frases
 
-
 listaf = []
 for i in range(len(lista)):
-    x = embed(lista[i][1])
-    listaf.append([lista[i][0], lista[i][1], x])
+    if len(lista[i]) == 3:
+        x = embed(lista[i][2])
+        listaf.append([lista[i][0], lista[i][1], lista[i][2], x])
+    else:
+        x = embed(lista[i][1])
+        listaf.append([lista[i][0], lista[i][1], x])
+
 
 def analisar_frase(frase):
     x = embed(frase)
     teste = []
     for i in listaf:
-        resi = np.inner(x, i[2])
-        teste.append([i[0], resi, i[1]])
+        if len(i) == 4:
+            resi = np.inner(x, i[3])
+            teste.append([i[0], resi, i[1], i[2]])
+        else:
+            resi = np.inner(x, i[2])
+            teste.append([i[0], resi, i[1]])
+    lista_ordenada = sorted(teste, key=lambda x: x[1], reverse=True)
+    return lista_ordenada
 
-    sorted_list = sorted(teste, key=lambda x: x[1], reverse=True)
-    return sorted_list
 
-def obter_resultado(final_result):
-    faixa_maior = final_result[0][1][0][0]
+def analisar_problema(frase_descricao):
+    x = embed(frase_descricao)
+    teste = []
+    for i in listaf:
+        if len(i) == 4:
+            resi = np.inner(x, i[3])
+            teste.append([i[1], resi, i[2]])
+    lista_ordenada = sorted(teste, key=lambda x: x[1], reverse=True)
+    return lista_ordenada
+
+
+def obter_resultado(lista_resultados_embeds):
+    faixa_maior = lista_resultados_embeds[0][1][0][0]
     faixa_menor = faixa_maior * 0.95
 
     resultado = []
-    for i in final_result:
+    for i in lista_resultados_embeds:
         if faixa_menor > i[1][0][0]:
             break
         resultado.append([i[0], i[1][0][0]])
-
     return resultado
 
-def conversar():
-    frase_usuario = input("Diga o problema do seu veículo: ")
-    resultado_inicial = obter_resultado(analisar_frase(frase_usuario))
+# frase = "A bateria desliga toda vez que uso o limpador de parabrisa"
+# print(obter_resultado(analisar_frase(frase)))
+# if obter_resultado(analisar_frase(frase))[0][0] == 'descrição de problema':
+#     print(obter_resultado(analisar_problema(frase)))
 
-    print(resultado_inicial)
-    if resultado_inicial[0][0] == 'saudação':
-        print("Saudações!")
-    else: 
-        if len(resultado_inicial) == 0 or resultado_inicial[0][1] < 0.75:
-            frase_usuario2 = input("Dê mais detalhes ou digite o seu problema de outra forma: ")
-            frase_completa = frase_usuario + " " + frase_usuario2
-            resultado_final = analisar_frase(frase_completa)
-            resultado_final = obter_resultado(resultado_final)
+def verificar_mensagem_gerar_resposta(mensagem_usuario):
+    resultado = obter_resultado(analisar_frase(mensagem_usuario))[0][0]
 
-            if len(resultado_final) == 0 or resultado_final[0][1] < 0.7:
-                print("Você parece ter um problema de", resultado_inicial[0], ". Te encaminharei para um Parceiro para análise precisa.")
-            else:
-                print("Com base nas informações adicionais, o problema parece ser", resultado_final[0])
-        else:
-            print("Com base no seu input, o problema parece ser", resultado_inicial[0])
+    if resultado == 'descrição de problema':
+        categoria_de_problema = obter_resultado(analisar_problema(mensagem_usuario))[0][0]
+        return f"Você parece ter um problema de {categoria_de_problema}"
+    elif resultado == 'saudação':
+        return "Olá, eu sou o AutoCare Bot! Como posso te ajudar?"
+    elif resultado == 'positivo' or 'negativo':
+        return "Ok."
 
-while True:
-    conversar()
+print(verificar_mensagem_gerar_resposta("Olá meu querido"))
+print(verificar_mensagem_gerar_resposta("A bateria desliga toda vez que uso o limpador de parabrisa"))
+print(verificar_mensagem_gerar_resposta("Sim"))
